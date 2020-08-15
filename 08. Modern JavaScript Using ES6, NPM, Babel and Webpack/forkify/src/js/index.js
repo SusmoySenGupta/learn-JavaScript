b86698301359b504b700b730b4e862687821114a
Susmoy <susmoy.cse@gmail.com>
2020-08-15 18:11:19 +0600

    Building the search controller

--- 08. Modern JavaScript Using ES6, NPM, Babel and Webpack/forkify/src/js/index.js
+++ 08. Modern JavaScript Using ES6, NPM, Babel and Webpack/forkify/src/js/index.js
@@ -1,8 +1,36 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getRecipies(query) {
-    const key = "b0080b30eed94f5cba5fcb96a1767051";
-    const res = await axios(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}&query=pasta`);
-    console.log(res);
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipies
+ */
+const state = {};
+
+const SearchController = async () => {
+    // 1. Get query from view
+    const query = 'pizza';
+
+    if (query) {
+        // 2. New search object and add to state
+        state.search = new Search(query);
+
+        // 3. Prepare UI for results
+
+        // 4. Search for recipies
+        await state.search.getRecipies();
+
+        // 5. Render results on UI
+        console.log(state.search.recipies);
+    }
 }
-getRecipies();
+
+document.querySelector('.search').addEventListener('submit', event => {
+    event.preventDefault();
+    SearchController();
+});
+
+const search = new Search('pizza');
+console.log(search);
+console.log(search.getRecipies());
--- 08. Modern JavaScript Using ES6, NPM, Babel and Webpack/forkify/src/js/models/Search.js
+++ 08. Modern JavaScript Using ES6, NPM, Babel and Webpack/forkify/src/js/models/Search.js
@@ -1,1 +1,18 @@
-export default 'I am Search.js';
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getRecipies() {
+        const key = "b0080b30eed94f5cba5fcb96a1767051";
+        try {
+            const res = await axios(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}&query=${this.query}`);
+            this.recipies = res.data.results;
+        } catch (error) {
+            alert(error);
+        }
+        //console.log(res);
+    }
+}
